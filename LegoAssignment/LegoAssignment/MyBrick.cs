4af0b5902f98a97a5a0196cdb45978c8e77ec4cd
Your Name <you@example.com>
2017-11-03 15:14:18 +1100

    - Started on connection method.

--- LegoAssignment/LegoAssignment/MyBrick.cs
+++ LegoAssignment/LegoAssignment/MyBrick.cs
@@ -11,5 +11,21 @@ namespace LegoAssignment
     class MyBrick
     {
 
+
     }
+
+    Brick brick = new Brick(new UsbCommunication());
+
+    public async void connect()
+    {
+        Console.WriteLine("Connecting, One Sec");
+
+        await brick.ConnectAsync();
+        brick.BrickChanged += OnBrickChanged;
+
+        await brick.DirectCommand.PlayToneAsync(50, 15, 666);
+
+        Console.WriteLine("Succsesfully Connected");
+    }
+
 }
