03ce017c4876c185f1307511e138dd25fba495a5
Taylor Anderson <tjandesn@gmail.com>
2017-11-03 14:34:22 +1100

    Initial Commit

---
+++ LegoAssignment/LegoAssignment/MyBrick.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lego.Ev3.Core;
+using Lego.Ev3.Desktop;
+
+namespace LegoAssignment
+{
+    class MyBrick
+    {
+
+    }
+}
