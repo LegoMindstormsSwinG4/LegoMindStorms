9bceded4b3bbf4cf023a0c2fd880d8d193dec1bb
Your Name <you@example.com>
2017-11-03 15:17:40 +1100

    - completed connect method

--- LegoAssignment/LegoAssignment/MyBrick.cs
+++ LegoAssignment/LegoAssignment/MyBrick.cs
@@ -10,22 +10,26 @@ namespace LegoAssignment
 {
     class MyBrick
     {
+        Brick brick = new Brick(new UsbCommunication());
 
+        public async void Connect()
+        {
+            Console.WriteLine("Connecting, One Sec");
 
+            await brick.ConnectAsync();
+            brick.BrickChanged += OnBrickChanged;
+
+            await brick.DirectCommand.PlayToneAsync(50, 15, 666);
+
+            Console.WriteLine("Succsesfully Connected");
+        }
+
+        private void OnBrickChanged(object sender, BrickChangedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
     }
 
-    Brick brick = new Brick(new UsbCommunication());
-
-    public async void connect()
-    {
-        Console.WriteLine("Connecting, One Sec");
-
-        await brick.ConnectAsync();
-        brick.BrickChanged += OnBrickChanged;
-
-        await brick.DirectCommand.PlayToneAsync(50, 15, 666);
-
-        Console.WriteLine("Succsesfully Connected");
-    }
+  
 
 }
